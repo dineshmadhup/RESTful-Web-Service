4fbc71c3655c68f8ddd1380925c3eea44f088f01
dineshmadhup <dineshmadhup@gmail.com>
2016-12-18 17:22:36 -0800

    RESTful Web Service CRUD Operation

---
+++ Module-6-productService-crud-operation/src/main/java/org/dinesh/softarica/productService/resources/ProductResource.java
@@ -0,0 +1,55 @@
+package org.dinesh.softarica.productService.resources;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.dinesh.softarica.productService.model.Product;
+import org.dinesh.softarica.productService.service.ProductService;
+
+@Path("/products")
+/* Adding Produces and Consumes annotation on class level */
+/* do not require at each individual method */
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class ProductResource {
+	
+	ProductService productService = new ProductService();
+	
+	@GET
+	//@Produces(MediaType.TEXT_PLAIN)
+	public List<Product> getProduct() {
+		return productService.getAllProducts();
+	}
+	
+	@POST
+	//@Consumes(MediaType.APPLICATION_JSON)
+	//@Produces(MediaType.APPLICATION_JSON)
+	public Product addProduct(Product product) {
+		return productService.addProduct(product);
+	}
+	
+	@PUT
+	@Path("/{productId}")
+	public Product updateProduct(@PathParam("productId") long pid, Product product) {
+		product.setPid(pid);
+		return productService.updateProduct(product);
+	}
+	
+	@DELETE
+	@Path("/{productId}")
+	public Product deleteProduct(@PathParam("productId") long pid) {
+		return productService.removeProduct(pid);
+	}
+	
+
+
+}
---
+++ Module-6-productService-crud-operation/src/main/java/org/dinesh/softarica/productService/service/ProductService.java
@@ -0,0 +1,52 @@
+package org.dinesh.softarica.productService.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.dinesh.softarica.productService.database.DatabaseClass;
+import org.dinesh.softarica.productService.model.Product;
+
+public class ProductService {
+
+	// Creating MAP with Key Long and value Product
+	private Map<Long, Product> myProducts = DatabaseClass.getProduct();
+	
+	// No-arg constructor
+	public ProductService() {
+
+		myProducts.put(1L, new Product(1, "IPhone7", "900"));
+	}
+
+	public List<Product> getAllProducts() {
+		return new ArrayList<Product>(myProducts.values());
+	}
+
+	 public Product getProduct(long id) {
+	 return myProducts.get(id);
+	 }
+	
+	
+	// Create operation
+	public Product addProduct(Product product) {
+		product.setPid(myProducts.size() + 1);
+		myProducts.put(product.getPid(), product);
+		return product;
+	}
+	
+	// Update Product
+	public Product updateProduct(Product product) {
+		if(product.getPid() <= 0) {
+			return null;
+		}
+		myProducts.put(product.getPid(), product);
+		return product;
+	}
+	
+	// Delete Operation
+	public Product removeProduct(long pid) {
+		return myProducts.remove(pid);
+	}
+	
+
+}
